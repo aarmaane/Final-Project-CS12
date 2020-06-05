e5eaf105923d52d0aca5f414a2ffafb0f2f3c3f0
Armaan Randhawa <aarmaane@gmail.com>
2020-06-04 22:17:01 -0400

    More boss sprites

--- Game Classes/Boss.java
+++ Game Classes/Boss.java
@@ -9,9 +9,19 @@ public class Boss extends Enemy{
     // Images
     private static Image bossHealthBar;
     private static Image[] idleSprites = new Image[5];
+    private static Image[] movingSprites = new Image[8];
+    private static Image[] chargingSprites = new Image[5];
+    private static Image[] attackSprites = new Image[9];
+    private static Image[] spinSprites = new Image[9];
+    private static Image[] deathSprites = new Image[6];
     // Initialization of Class
     public static void init(){
         idleSprites = Utilities.spriteArrayLoad(idleSprites, "Enemies/Boss/idle");
+        movingSprites = Utilities.spriteArrayLoad(movingSprites, "Enemies/Boss/moving");
+        chargingSprites = Utilities.spriteArrayLoad(chargingSprites, "Enemies/Boss/charging");
+        attackSprites = Utilities.spriteArrayLoad(attackSprites, "Enemies/Boss/attack");
+        spinSprites = Utilities.spriteArrayLoad(spinSprites, "Enemies/Boss/spin");
+        deathSprites = Utilities.spriteArrayLoad(deathSprites, "Enemies/Boss/death");
     }
 
     public Boss(String data) {
@@ -25,17 +35,40 @@ public class Boss extends Enemy{
     public void drawHealth(Graphics g, int levelOffset){
         g.drawString("BOSS HEALTH: " + health, 500, 400);
     }
+
+    @Override
+    public void updateMotion(Player player) {
+        velocityX = 0.01;
+        super.updateMotion(player);
+    }
+
     @Override
     public void updateSprite() {
-        spriteCount += 0.05;
-        if(spriteCount > idleSprites.length){
-            spriteCount = 0;
+        if(velocityX != 0){
+            spriteCount += 0.05;
+            if(spriteCount > movingSprites.length){
+                spriteCount = 0;
+            }
+        }
+        else{
+            spriteCount += 0.05;
+            if(spriteCount > idleSprites.length){
+                spriteCount = 0;
+            }
         }
     }
     // Getter methods
     @Override
     public Image getSprite() {
-        return idleSprites[(int) Math.floor(spriteCount)];
+        Image sprite;
+        int spriteIndex = (int)Math.floor(spriteCount);
+        if(velocityX != 0){
+            sprite = movingSprites[spriteIndex];
+        }
+        else{
+            sprite = idleSprites[spriteIndex];
+        }
+        return sprite;
     }
     @Override
     public Rectangle getHitbox() {
